96577f9e39268b3b262b14aa8f54aba17f615fb0
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-05 16:57:27 -0700

    aonther error corrected

--- 0x01-variables_if_else_while/9-print_combo.c
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -3,34 +3,31 @@
 /**
  * main - Entry point
  *
- * Description: This code will print out all the
- * numbers of base16 from 0
- * and will print a new line
+ * Description: This code printsall possible combinations of
+ * single-digit numbers. each spe by a space and a comma.
+ * prints to stdout
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int hex1start = 0;
-	int hex1end = 9;
-	int hexAstart = 97;
-	int hexAend = 102;
+	int start = 0;
+	int end = 9;
 
 	int i;
-	int j;
 
-	for (i = hex1start; i <= hex1end; ++i)
+	for (i = start; i <= end; ++i)
 	{
 		putchar(i + '0');
-	}
-
-	for (j = hexAstart; j <= hexAend; ++j)
-	{
-		putchar(j);
+		if (i < end)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
 
 	return (0);
+
 }
