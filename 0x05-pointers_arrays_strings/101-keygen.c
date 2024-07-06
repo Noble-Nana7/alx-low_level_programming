431f35c721bcf75fd154e8a141e7b71dd09a3769
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-06 09:17:45 -0700

    commit 101 again

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,27 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define PASSWORD_LENGTH 10
+
 int main(void)
 {
 	int i;
-	int password_length = 10;
-	char password[password_length + 1];
-	
-	// Seed the random number generator with the current time
-	srand(time(0));
-	
-	// Generate a random password
-	for (i = 0; i < password_length; i++)
+	char password[PASSWORD_LENGTH + 1];
+
+	/* Seed the random number generator with the current time */
+	srand((unsigned int)time(NULL));
+
+	/* Generate a random password */
+	for (i = 0; i < PASSWORD_LENGTH; i++)
 	{
-		// Generate a random character from ASCII values 33 to 126
+		/* Generate a random character from ASCII values 33 to 126 */
 		password[i] = rand() % 94 + 33;
 	}
-	
-	// Null-terminate the password
-	password[password_length] = '\0';
-	
-	// Print the generated password
+
+	/* Null-terminate the password */
+	password[PASSWORD_LENGTH] = '\0';
+
+	/* Print the generated password */
 	printf("Generated password: %s\n", password);
 
 	return 0;
 }
+
