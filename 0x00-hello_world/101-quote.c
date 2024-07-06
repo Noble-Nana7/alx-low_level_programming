3877ead65799dec22a2d1fcd3d5b87cd107c2ec5
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-05 18:18:41 -0700

    another fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - Entry point of the program
  *
  * Description:
- *   This program prints the message 
+ *   This program prints the message:
  *   "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  *   to the standard error (stderr) using the write system call.
  *   It does not use printf or puts.
@@ -15,10 +15,10 @@
 int main(void)
 {
     /* Message to be printed */
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
     /* Using the write system call to print to stderr (file descriptor 2) */
-	write(2, message, 59);
+	write(2, text, 59);
 	/* 59 is the length of the message excluding the null terminator */
 
 	return (1);
