1256fcc374f278326ed04e459d4c054c7e4cec54
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-06 05:35:11 -0700

    commit 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase using _putchar
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+	char z = 'z';
+	char 'i';
+
+	for (i = a; i <= z; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
