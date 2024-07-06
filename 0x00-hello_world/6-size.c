02753ab5b913a14252c4c376c788275e3943d843
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-05 18:07:38 -0700

    another correct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,11 @@
  * main - Entry point of the program
  *
  * This program prints the sizes of various types:
- *   - char
- *   - int
- *   - long
- *   - long long
- *   - float
- *   - double
- *   - long double
+ *   - a char
+ *   - an int
+ *   - a long int
+ *   - a long long int
+ *   - a float
  * These sizes are printed in bytes using the sizeof operator.
  *
  * Return:
@@ -18,11 +16,12 @@
  */
 int main(void)
 {
-	printf("Size of char: %zu byte(s)\n", sizeof(char));
-	printf("Size of int: %zu byte(s)\n", sizeof(int));
-	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
+
