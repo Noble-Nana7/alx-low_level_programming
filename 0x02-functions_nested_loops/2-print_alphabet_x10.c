9cabc07db244cd3e7f99e12895455868cd464885
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-06 06:03:21 -0700

    commit 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times,
+ * followed by a newline
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	}
+
+	_putchar('\n');
+}
