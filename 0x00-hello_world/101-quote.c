f7d57fcf826684ca67c55924d83892870f8c71ad
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-05 18:13:57 -0700

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ *   This program prints the message 
+ *   "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *   to the standard error (stderr) using the write system call.
+ *   It does not use printf or puts.
+ *
+ * Return:
+ *   Always returns 1 to indicate an error condition.
+ */
+int main(void)
+{
+    // Message to be printed
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    // Using the write system call to print to stderr (file descriptor 2)
+    write(2, message, 59);
+	// 59 is the length of the message excluding the null terminator
+
+    return 1;
+}
