f03021e7755c4105013b0288c8de7343de6ab5c8
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-06 09:08:14 -0700

    commit a couple

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse , followed by
+ * a new line.
+ * @s: Pointer to the string.
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	while (length > 0)
+	{
+		s--;
+		_putchar(*s);
+		length--;
+	}
+	_putchar('\n');
+}
