b79ef84239ce7d631889c09b6b0a1d2be8443308
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-05 17:41:44 -0700

    betty!!

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,42 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: This program generates and prints all possible combinations
- *              of two two-digit numbers, separated by a comma and space. The combinations
- *              are printed in ascending order.
+ * Description:
+ *     This program generates and prints all possible combinations
+ *     of two two-digit numbers, separated by a comma and space.
  *
- * Return: Always 0 (Success)
+ *     The combinations are printed in ascending order.
+ *
+ * Return:
+ *     Always returns 0 to indicate successful execution.
  */
-#include <stdio.h>
 
 int main(void)
 {
-    int a, b;
+	int a, b;
 
-    for (a = 0; a < 100; a++)
-    {
-        for (b = a + 1; b < 100; b++)
-        {
-            putchar((a / 10) + '0');
-            putchar((a % 10) + '0');
-            putchar(' ');
-            putchar((b / 10) + '0');
-            putchar((b % 10) + '0');
+	for (a = 0; a < 100; a++)
+	{
+		for (b = a + 1; b < 100; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
 
-            if (a != 98 || b != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
 
-    putchar('\n');
-    return 0;
+			if (a != 98 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');  /* Print a newline character at the end */
+
+	return (0);
 }
-
