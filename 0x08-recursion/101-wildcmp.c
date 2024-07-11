9a2cc5ebbde7d7e99958a576ab51dba967c5139b
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-11 15:02:00 -0700

    commit 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * wildcmp - Compares two strings with wildcard support.
- * @s1: First string.
- * @s2: Second string with wildcard (*).
- *
- * Return: 1 if strings can be considered identical, 0 otherwise.
- */
 int wildcmp(char *s1, char *s2)
 {
-	/* If we reach the end of both strings, they are identical */
-	if (*s1 == '\0' && (*s2 == '\0' || (*s2 == '*' && *(s2 + 1) == '\0')))
+	/* Base case: If both strings reach the end */
+	if (*s1 == '\0' && *s2 == '\0')
 	{
-		return 1;
+		return (1);
 	}
 
-	/* If the current characters match or s2 has a wildcard */
-	if (*s1 == *s2 || *s2 == '*')
+	/* If s2 contains '*' */
+	if (*s2 == '*')
 	{
-		/* Move to the next characters in both strings */
-		return wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1);
+		/* Move to the next character in s2 or consider '*' matching empty string */
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
 
-	/* If the characters don't match and there is no wildcard */
-	return 0;
+	/* If characters match, move to the next character in both strings */
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	/* If characters do not match */
+	return (0);
 }
