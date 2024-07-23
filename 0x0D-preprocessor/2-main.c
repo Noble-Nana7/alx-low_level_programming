321876e485354a9c185d01d6ddf2961fa01dd566
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-23 15:22:21 -0700

    PRINT NAME OF FILE USING DEFMACRO

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main funtion to print name of file using a macro
+ *
+ * Return: Always 0 (Sucsess)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
