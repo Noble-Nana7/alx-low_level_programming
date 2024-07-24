89818800036e0d5b783781191ac8420d44b7ad3c
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-24 09:11:33 -0700

    DOGS FRED!

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees a dog.
+ * @d: Pointer to the dog_t structure to free.
+ *
+ * Description:
+ * This function frees the memory allocated for a dog_t structure, including 
+ * the memory allocated for the name and owner members.
+ */
+void free_dog(dog_t *d)
+{
+	/* Check if the pointer d is not NULL */
+	if (d != NULL) {
+		/* Free the memory allocated for the name */
+		free(d->name);
+		
+		/* Free the memory allocated for the owner */
+		free(d->owner);
+		
+		/* Free the memory allocated for the dog_t structure */
+		free(d);
+	}
+}
+
