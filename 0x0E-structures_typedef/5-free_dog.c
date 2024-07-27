97287cd48330c563a2d45b70f25900eadb7197cc
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-26 20:03:55 -0700

    TRAILING WHITWSPACE

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,20 +6,20 @@
  * @d: Pointer to the dog_t structure to free.
  *
  * Description:
- * This function frees the memory allocated for a dog_t structure, including 
+ * This function frees the memory allocated for a dog_t structure, including
  * the memory allocated for the name and owner members.
  */
 void free_dog(dog_t *d)
 {
 	/* Check if the pointer d is not NULL */
-	if (d != NULL) 
+	if (d != NULL)
 	{
 		/* Free the memory allocated for the name */
 		free(d->name);
-		
+
 		/* Free the memory allocated for the owner */
 		free(d->owner);
-		
+
 		/* Free the memory allocated for the dog_t structure */
 		free(d);
 	}
