5b96cc6641e154405e1aa5b7831e071c7aff02a0
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-08-05 19:52:50 -0700

    Again

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,20 +11,20 @@
  * return -1. If size <= 0, return -1.
  */
 
-int int_index(int *array, int sise, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL || size <== 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 	{
 		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[1]) != 0)
+		if (cmp(array[i]) != 0)
 		{
-			return (1);
+			return (i);
 		}
 	}
 	return (-1);
